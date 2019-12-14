5cea6aa5b282f9b5fa308628d3c382b0d314e96b
Tim van Dijen <tvdijen@gmail.com>
2019-12-14 13:42:23 +0100

    Fix InvalidScope

--- www/about2expire.php
+++ www/about2expire.php
@@ -34,35 +34,35 @@ $t->data['netId'] = $state['netId'];
 
 if ($daysleft == 0) {
     # netid will expire today
-    $this->data['header'] = $this->t('{expirycheck:expwarning:warning_header_today}', [
-                                '%NETID%' => htmlspecialchars($this->data['netId'])
+    $t->data['header'] = $t->t('{expirycheck:expwarning:warning_header_today}', [
+                                '%NETID%' => htmlspecialchars($->data['netId'])
                         ]);
-    $this->data['warning'] = $this->t('{expirycheck:expwarning:warning_today}', [
-                                '%NETID%' => htmlspecialchars($this->data['netId'])
+    $t->data['warning'] = $t->t('{expirycheck:expwarning:warning_today}', [
+                                '%NETID%' => htmlspecialchars($t->data['netId'])
                         ]);
 } elseif ($daysleft == 1) {
     # netid will expire in one day
 
-    $this->data['header'] = $this->t('{expirycheck:expwarning:warning_header}', [
-                                '%NETID%' => htmlspecialchars($this->data['netId']),
-                                '%DAYS%' => $this->t('{expirycheck:expwarning:day}'),
+    $t->data['header'] = $t->t('{expirycheck:expwarning:warning_header}', [
+                                '%NETID%' => htmlspecialchars($t->data['netId']),
+                                '%DAYS%' => $t->t('{expirycheck:expwarning:day}'),
                                 '%DAYSLEFT%' => htmlspecialchars($daysleft),
                         ]);
-    $this->data['warning'] = $this->t('{expirycheck:expwarning:warning}', [
-                                '%NETID%' => htmlspecialchars($this->data['netId']),
-                                '%DAYS%' => $this->t('{expirycheck:expwarning:day}'),
+    $t->data['warning'] = $t->t('{expirycheck:expwarning:warning}', [
+                                '%NETID%' => htmlspecialchars($t->data['netId']),
+                                '%DAYS%' => $t->t('{expirycheck:expwarning:day}'),
                                 '%DAYSLEFT%' => htmlspecialchars($daysleft),
                         ]);
 } else {
     # netid will expire in next <daysleft> days
-    $this->data['header'] = $this->t('{expirycheck:expwarning:warning_header}', [
-                                '%NETID%' => htmlspecialchars($this->data['netId']),
-                                '%DAYS%' => $this->t('{expirycheck:expwarning:days}'),
+    $t->data['header'] = $t->t('{expirycheck:expwarning:warning_header}', [
+                                '%NETID%' => htmlspecialchars($t->data['netId']),
+                                '%DAYS%' => $t->t('{expirycheck:expwarning:days}'),
                                 '%DAYSLEFT%' => htmlspecialchars($daysleft),
                         ]);
-    $this->data['warning'] = $this->t('{expirycheck:expwarning:warning}', [
-                                '%NETID%' => htmlspecialchars($this->data['netId']),
-                                '%DAYS%' => $this->t('{expirycheck:expwarning:days}'),
+    $t->data['warning'] = $t->t('{expirycheck:expwarning:warning}', [
+                                '%NETID%' => htmlspecialchars($t->data['netId']),
+                                '%DAYS%' => $t->t('{expirycheck:expwarning:days}'),
                                 '%DAYSLEFT%' => htmlspecialchars($daysleft),
                         ]);
 }

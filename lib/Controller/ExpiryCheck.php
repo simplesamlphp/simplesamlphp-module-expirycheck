90ad75c0ac18f89ca4ee95c3188f6a1235f8ca9f
Tim van Dijen <tvdijen@gmail.com>
2021-08-22 16:05:30 +0200

    Migrate translations

--- lib/Controller/ExpiryCheck.php
+++ lib/Controller/ExpiryCheck.php
@@ -99,59 +99,7 @@ class ExpiryCheck
         $t->data['yesTarget'] = Module::getModuleURL('expirycheck/about2expire.php');
         $t->data['yesData'] = ['StateId' => $stateId];
         $t->data['expireOnDate'] = $state['expireOnDate'];
-        $t->data['netId'] = $state['netId'];
-
-        if ($daysleft === 0) {
-            # netid will expire today
-            $t->data['header'] = $t->t(
-                '{expirycheck:expwarning:warning_header_today}',
-                [
-                    '%NETID%' => htmlspecialchars($t->data['netId'])
-                ]
-            );
-            $t->data['warning'] = $t->t(
-                '{expirycheck:expwarning:warning_today}',
-                [
-                    '%NETID%' => htmlspecialchars($t->data['netId'])
-                ]
-            );
-        } elseif ($daysleft === 1) {
-            # netid will expire in one day
-            $t->data['header'] = $t->t(
-                '{expirycheck:expwarning:warning_header}',
-                [
-                    '%NETID%' => htmlspecialchars($t->data['netId']),
-                    '%DAYS%' => $t->t('{expirycheck:expwarning:day}'),
-                    '%DAYSLEFT%' => htmlspecialchars($daysleft),
-                ]
-            );
-            $t->data['warning'] = $t->t(
-                '{expirycheck:expwarning:warning}',
-                [
-                    '%NETID%' => htmlspecialchars($t->data['netId']),
-                    '%DAYS%' => $t->t('{expirycheck:expwarning:day}'),
-                    '%DAYSLEFT%' => htmlspecialchars($daysleft),
-                ]
-            );
-        } else {
-            # netid will expire in next <daysleft> days
-            $t->data['header'] = $t->t(
-                '{expirycheck:expwarning:warning_header}',
-                [
-                    '%NETID%' => htmlspecialchars($t->data['netId']),
-                    '%DAYS%' => $t->t('{expirycheck:expwarning:days}'),
-                    '%DAYSLEFT%' => htmlspecialchars($daysleft),
-                ]
-            );
-            $t->data['warning'] = $t->t(
-                '{expirycheck:expwarning:warning}',
-                [
-                    '%NETID%' => htmlspecialchars($t->data['netId']),
-                    '%DAYS%' => $t->t('{expirycheck:expwarning:days}'),
-                    '%DAYSLEFT%' => htmlspecialchars($daysleft),
-                ]
-            );
-        }
+        $t->data['netId'] = htmlspecialchars($state['netId']);
 
         return $t;
     }
@@ -170,7 +118,7 @@ class ExpiryCheck
 
         $stateId = $request->get('StateId');
         if ($stateId === null) {
-            throw new \SimpleSAML\Error\BadRequest('Missing required StateId query parameter.');
+            throw new Error\BadRequest('Missing required StateId query parameter.');
         }
 
         /** @psalm-var array $state */
